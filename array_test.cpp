a63de42cfb5d1edc3bbfa390f670bc1c9eac1762
theoka <usingalovelytune@gmail.com>
2015-01-05 06:05:21 -0500

    Cleaned up, now decrements output length.

--- array_test.cpp
+++ array_test.cpp
@@ -1,42 +1,47 @@
-    /*
-     * Algorithm to print every possible
-     * combination of characters within
-     * a set length. In this case, a 2d
-     * array via manipulating the y
-     * coordinates.
-     */
-     
-    #include<iostream>
-    int main()
-    {
-        int anArray[3][4]={
-            { 1, 1, 1, 1}, // row 0
-            { 2, 2, 2, 2}, // row 1
-            { 3, 3, 3, 3} // row 2
-        };
-        int mxHt=2; //highest y coordinate
-        int index=3; //highest x coordinate
-        int yCoords[]={0,0,0,0}; //y coords are the
-                                 //only thing that change
-        bool run=true; //outer loop bool
-        bool exit=false; //innter loop bool
-        while(run){    
-            for(int i=0;i<4;i++)
-                std::cout<<anArray[yCoords[i]][i];
-            std::cout<<'\n';
-     
-            while(!exit){
-                if(yCoords[index]==mxHt){
-                    yCoords[index]=0;
-                    if(index>0) index--;
-                    else run = !run;
-                }
-                if(yCoords[index]<mxHt){
-                    yCoords[index]++;
-                    index=3;
-                    exit = !exit;
-                }
-            } exit = !exit;
+/*
+ * Algorithm to print every possible
+ * combination of characters within
+ * a length range. In this case, a 2d
+ * array via manipulating the y
+ * coordinates.
+ */
+ 
+#include<iostream>
+int main()
+{
+    int anArray[3][4]={
+        {1, 1, 1, 1},
+        {2, 2, 2, 2},
+        {3, 3, 3, 3}};
+
+    int minLen=2;
+    int maxLen=4;
+    int cut=0; //how much is cut from output
+    int mxHt=2; //highest y coordinate
+    int index=3; //highest x coordinate
+    int x=index;
+    int yCoords[]={0,0,0,0}; //y coords are the
+                             //only thing that change
+    bool run=true; //outer loop bool
+    while(true){    
+        for(int i=cut;i<4;i++)
+            std::cout<<anArray[yCoords[i]][i];
+        std::cout<<'\n';
+ 
+        while(true){
+            if(yCoords[index]==mxHt){
+                yCoords[index]=0;
+                if(index>0) index--;
+                else if(cut<maxLen-minLen) cut++;
+                else goto end;
+            }
+            if(yCoords[index]<mxHt){
+                yCoords[index]++;
+                index=3;
+                break;
+            }
         }
-        return 0;
     }
+    end:
+    return 0;
+}
