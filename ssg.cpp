78c8e89a5bf9e5712c6b5d9339e3c735cc495522
theoka <usingalovelytune@gmail.com>
2015-01-07 03:26:38 -0500

    Time to plug in array_test.

--- ssg.cpp
+++ ssg.cpp
@@ -16,9 +16,9 @@
 using namespace std;
 
 //sets parameters by given args
-void setParams(int argc, char *argv[]);
+int setParams(int argc, char *argv[]);
 //outputs based on givem parameters
-void buildOutput();
+void buildOutput(int height);
 
 int minLen,maxLen;
 bool params[4]; //letters, caps, numbers, special
@@ -30,49 +30,82 @@ int main(int argc, char *argv[])
             <<"arg1 arg2 arg3...\n\n"
             <<"l - letters\nc - caps\nn - numbers\n"
             <<"s - special characters\n";
-    else{
-        setParams(argc,argv);
-        buildOutput();
-    }
+    else buildOutput(setParams(argc,argv));
     return 0;
 }
 
-void setParams(int argc, char *argv[])
+int setParams(int argc, char *argv[])
 {
+    int height=0;
     //capture max and min length
     minLen = atoi(argv[1]);
     maxLen = atoi(argv[2]);
     //clear parameters before setting them
-    for(int i=0;i<4;i++)
-        params[i]=false;
+    for(int i=0;i<4;i++) params[i]=false;
     //set parameter array based on args
     for(int i=3;i<argc;i++){
-        if(*argv[i]=='l') params[0]=true;
-        if(*argv[i]=='c') params[1]=true;
-        if(*argv[i]=='n') params[2]=true;
-        if(*argv[i]=='s') params[3]=true;
+        if(*argv[i]=='l'){
+            params[0]=true;
+            height+=26;
+        }
+        if(*argv[i]=='c'){
+            params[1]=true;
+            height+=26;
+        }
+        if(*argv[i]=='n'){
+            params[2]=true;
+            height+=10;
+        }
+        if(*argv[i]=='s'){
+            params[3]=true;
+            height+=30;
+        }
     }
+    return height;
 }
 
-void buildOutput()
+//I hate this entire function right now
+void buildOutput(int height)
 {
+    char output[height][maxLen]; //the whole point of this
+    int fill=0; //keep up with how full output arr is
     //create and fill alphabet array(s)
     if(params[0]||params[1]){
-        char *alphabet[26];
-        *alphabet[0]='a';
-        for(char c='a';c<='z';c++)
-            *alphabet[c-'a'] = c;
+        char alphabet[26];
+        alphabet[0]='a';
+        for(char c='a';c<='z';c++) alphabet[c-'a'] = c;
+        for(int i=0;i<maxLen;i++)
+            for(int j=fill;j<height;j++)
+                output[j][i]=alphabet[j];
+        fill++;
         if(params[1]){
             locale loc;
-            char *caps[26];
+            char caps[26];
             for(int i=0;i<26;i++)
-                *caps[i]=toupper(*alphabet[i],loc);               
+                caps[i]=toupper(alphabet[i],loc);               
+            for(int i=0;i<maxLen;i++)
+                for(int j=fill;j<height;j++)
+                    output[j][i]=caps[j];
+            fill++;
         }
+    }
+    //numbers
+    if(params[2]){
+        char numeros[]={0,1,2,3,4,5,6,7,8,9};
+        for(int i=0;i<maxLen;i++)
+            for(int j=fill;j<height;j++)
+                output[j][i]=numeros[j];
+        fill++;
     }
     //special characters array
     if(params[3]){
         char specials[]={'?', '!', '@', '#', '$', '%', '^', '&',
                          '*', '(', ')', '{', '}', '|', '\\', '/',
-                         '_', '-', '=', '+', '~', '`'};
+                         '_', '-', '=', '+', '~', '`', '\'', '\"',
+                         ':', ';', '<', '>', ',', '.'};
+        for(int i=0;i<maxLen;i++)
+            for(int j=fill;j<height;j++)
+                output[j][i]=specials[j];
     }
 }
+
